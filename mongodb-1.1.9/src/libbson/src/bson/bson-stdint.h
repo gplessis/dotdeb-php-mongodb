5e3fb6769deb66968d3acd3aac4a19264fd8160e
Guillaume Plessis <gui@dotdeb.org>
2016-10-20 18:48:52 +0000

    Imported Upstream version 1.1.9

--- mongodb-1.1.9/src/libbson/src/bson/bson-stdint.h
+++ mongodb-1.1.9/src/libbson/src/bson/bson-stdint.h
@@ -2,7 +2,7 @@
 #define ___SRC_LIBBSON_SRC_BSON_BSON_STDINT_H 1
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H " "
-/* generated using a gnu compiler version cc (Ubuntu 5.3.1-14ubuntu2.1) 5.3.1 20160413 Copyright (C) 2015 Free Software Foundation, Inc. This is free software; see the source for copying conditions. There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. */
+/* generated using a gnu compiler version cc (Ubuntu 5.4.0-6ubuntu1~16.04.2) 5.4.0 20160609 Copyright (C) 2015 Free Software Foundation, Inc. This is free software; see the source for copying conditions. There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. */
 
 #include <stdint.h>
 
